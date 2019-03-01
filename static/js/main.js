eec3d3c6db7a0a646fb5f79376360265274ca525
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-01 18:47:01 +0100

    changed rotation table to detailed data table

--- static/js/main.js
+++ static/js/main.js
@@ -5,22 +5,22 @@ var config = {
     }
 }
 
-var tab = []
+var level = []
 
 function create() {
     $("#map").find(".hexagon").remove()
     var quantity = $("#select").val()
     console.log(`creating ${quantity} hexagons`);
     var id = 0;
-    tab = []
-    for (var j = 0; j < quantity; j++) {
-        for (var i = 0; i < quantity; i++) {
+    level = []
+    for (let j = 0; j < quantity; j++) {
+        for (let i = 0; i < quantity; i++) {
             let img = $(`<div class='hexagon' id='hexagon${id}'>`)
             img
                 .css("position", "absolute")
                 .css("background-image", "url('/static/img/hexagon.png')")
                 .css("background-size", "contain")
-                .css("left",  ((86.25 + config.hexagons.spaces +1) * j) + config.hexagons.offset)
+                .css("left", ((86.25 + config.hexagons.spaces + 1) * j) + config.hexagons.offset)
             if (j % 2 == 0) {
                 img
                     .css("top", ((100 + config.hexagons.spaces) * i) + config.hexagons.offset)
@@ -30,37 +30,43 @@ function create() {
                     .css("top", ((100 + config.hexagons.spaces) * i + 50) + config.hexagons.offset)
             }
 
-
             img.click(function () {
                 var id = this.id.substr(7, this.id.length - 2)
                 console.log(`hexagon ${id} clicked`);
-                console.log(tab[id]);
-                if (tab[id] == undefined) {
-                    console.log("doesnt exist yet");
+                console.log(`col: ${j}, row: ${i}`);
+                var entry = level.find(hexagon => hexagon.id == id)
+                if (entry == undefined) {
+                    console.log("doesnt exist yet - creating");
                     var arrow = $("<img src='/static/img/arrow.png' class='arrow' id='arrow" + id + "'>")
                     $("#hexagon" + id).append(arrow)
-                    tab[id] = 0
                     var div = $("<div id='displayDiv" + id + "' class='displayDiv'>")
                     $("#hexagon" + id).append(div)
+                    level.push(
+                        {
+                            id: id,
+                            col: j,
+                            row: i,
+                            dir: 0,
+                        }
+                    )
+                    entry = level.find(hexagon => hexagon.id == id)
                 }
                 else {
-                    console.log("exists");
-                    if (tab[id] == 5) {
-                        tab[id] = 0
+                    console.log("exists - updating");
+                    if (entry.dir == 5) {
+                        entry.dir = 0
                     }
                     else {
-                        tab[id]++
+                        entry.dir++
                     }
                 }
-                $("#displayDiv" + id).text(tab[id])
-                img.css("transform", "rotate(" + tab[id] * 60 + "deg)")
+                $("#displayDiv" + id).text(entry.dir)
+                img.css("transform", "rotate(" + entry.dir * 60 + "deg)")
             })
-
 
             img.appendTo($("#map"))
             id++
         }
-
     }
 }
 
