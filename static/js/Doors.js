6542ea21bb953c05e08e6f653a357b03f07d382f
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-13 11:32:31 +0100

    fully working dirout doors

--- static/js/Doors.js
+++ static/js/Doors.js
@@ -1,10 +1,10 @@
-class Doors{
-    constructor(){
+class Doors {
+    constructor() {
         var container = new THREE.Object3D()
         var part1 = new THREE.Mesh(Settings.hexDoorsgeometry, Settings.hexDoorsmaterial);
         var part2 = new THREE.Mesh(Settings.hexDoorsgeometry, Settings.hexDoorsmaterial);
-        part1.position.x = 40
-        part2.position.x = -40
+        part1.position.x = (Settings.radius - (Settings.radius * 0.4) - (Settings.radius * 0.3))
+        part2.position.x = -(Settings.radius - (Settings.radius * 0.4) - (Settings.radius * 0.3))
         container.add(part1)
         container.add(part2)
         return container
