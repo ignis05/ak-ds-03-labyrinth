6f471eac5c20a2940873bd4ce8179082906512e1
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-03-13 11:00:21 +0100

    rendering only saved hexagons

--- static/js/Level.js
+++ static/js/Level.js
@@ -2,6 +2,7 @@ class Level {
     constructor() {
         this.net = new Net()
         this.map = null
+        this.hexagons = []
         this.net.loadlevel().then(map => {
             this.map = map
             console.log(this.map);
@@ -17,20 +18,17 @@ class Level {
 
     create() {
         console.log("creating level");
-        var models = []
-        console.log(this.map.level);
-        for (let j = 0; j < this.map.size; j++) {
-            for (let i = 0; i < this.map.size; i++) {
-                let model = new Hex3D()
-                scene.add(model)
-                model.position.x = Settings.radius * 2 * j
-                if (j % 2 == 0) {
-                    model.position.z = ((Settings.radius * 2) * i)
-                }
-                else {
-                    model.position.z = ((Settings.radius * 2) * i + Settings.radius)
-                }
+        for (var location of this.map.level) {
+            let model = new Hex3D()
+            scene.add(model)
+            model.position.x = Settings.radius * 2 * location.row
+            if (location.row % 2 == 0) {
+                model.position.z = ((Settings.radius * 2) * location.col)
             }
+            else {
+                model.position.z = ((Settings.radius * 2) * location.col + Settings.radius)
+            }
+            this.hexagons.push(model)
         }
     }
 }
